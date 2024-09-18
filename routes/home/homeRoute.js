9553cb017f9b97cd89f71a3380faccbda96a1d13
olaoluwa <146534182+olaoluwajo@users.noreply.github.com>
2024-09-18 04:00:39 +0100

    added range

--- routes/home/homeRoute.js
+++ routes/home/homeRoute.js
@@ -5,5 +5,6 @@ const router = require("express").Router();
 
 router.get("/get-categories", homeControllers.get_categories);
 router.get("/get-products", homeControllers.get_products);
+router.get("/price-range-latest-product", homeControllers.price_range_product);
 
 module.exports = router;
