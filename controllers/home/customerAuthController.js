e1063ea140ee7a1c6140b5296350bccd67a66a83
olaoluwa <146534182+olaoluwajo@users.noreply.github.com>
2024-09-22 01:59:20 +0100

    customer register and login done

--- controllers/home/customerAuthController.js
+++ controllers/home/customerAuthController.js
@@ -3,6 +3,7 @@ const { responseReturn } = require("../../utils/response");
 const bcrypt = require("bcrypt");
 const sellerCustomerModel = require("../../models/chat/sellerCustomerModel");
 const { createToken } = require("../../utils/tokenCreate");
+const { compare } = require("bcrypt");
 
 class customerAuthController {
   customer_register = async (req, res) => {
@@ -37,6 +38,47 @@ class customerAuthController {
           message: "User Register Successfull",
           token,
         });
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  // End Method
+
+  customer_login = async (req, res) => {
+    console.log(req.body);
+    const { email, password } = req.body;
+    try {
+      const customer = await customerModel
+        .findOne({ email })
+        .select("+password");
+      if (customer) {
+        // console.log("Stored hash in DB:", customer.password);
+        // console.log("Password from user:", password);
+        // const match = await bcrypt.compare(password, customer.password);
+        // Compare the password
+        const match = await compare(password, customer.password);
+        console.log("Password match:", match);
+
+        if (match) {
+          const token = await createToken({
+            id: customer.id,
+            name: customer.name,
+            email: customer.email,
+            method: customer.method,
+          });
+          res.cookie("customerToken", token, {
+            expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+          });
+          responseReturn(res, 201, {
+            message: "Customer Login Successfull",
+            token,
+          });
+        } else {
+          responseReturn(res, 404, { error: "Password Wrong" });
+        }
+      } else {
+        responseReturn(res, 404, { error: "Email Not Found" });
       }
     } catch (error) {
       console.log(error.message);
--- routes/home/customerAuthRoutes.js
+++ routes/home/customerAuthRoutes.js
@@ -4,5 +4,6 @@ router.post(
   "/customer/customer-register",
   customerAuthController.customer_register
 );
+router.post("/customer/customer-login", customerAuthController.customer_login);
 
 module.exports = router;
