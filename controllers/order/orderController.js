db5006711fab237f3baa491f93bb781d47da2f3c
olaoluwa <146534182+olaoluwajo@users.noreply.github.com>
2024-09-27 03:24:56 +0100

    orders contollers and routes done ongoing

--- controllers/order/orderController.js
+++ controllers/order/orderController.js
@@ -3,6 +3,9 @@ const { responseReturn } = require("../../utils/response");
 const customerOrder = require("../../models/customerOrder");
 const cartModel = require("../../models/cartModel");
 const authOrderModel = require("../../models/authOrder");
+const {
+  mongo: { ObjectId },
+} = require("mongoose");
 
 class orderController {
   paymentCheck = async (id) => {
@@ -43,7 +46,7 @@ class orderController {
       // console.log("proLENGTH", pro.length);
       for (let j = 0; j < pro.length; j++) {
         const tempCusPro = pro[j].productInfo;
-        
+
         tempCusPro.quantity = pro[j].quantity;
         // console.log("tempCusPro", tempCusPro.quantity);
         customerOrderProduct.push(tempCusPro);
@@ -100,6 +103,83 @@ class orderController {
     } catch (error) {
       console.log("ERROR", error.message);
       // responseReturn(res, 500, { error: error.message });
+    }
+  };
+  // End Method
+
+  get_customer_dashboard_data = async (req, res) => {
+    const { userId } = req.params;
+    // console.log(userId);
+    try {
+      const recentOrders = await customerOrder
+        .find({
+          customerId: new ObjectId(userId),
+        })
+        .limit(5);
+      const pendingOrder = await customerOrder
+        .find({
+          customerId: new ObjectId(userId),
+          delivery_status: "pending",
+        })
+        .countDocuments();
+      const totalOrder = await customerOrder
+        .find({
+          customerId: new ObjectId(userId),
+        })
+        .countDocuments();
+      const cancelledOrder = await customerOrder
+        .find({
+          customerId: new ObjectId(userId),
+          delivery_status: "cancelled",
+        })
+        .countDocuments();
+      responseReturn(res, 200, {
+        recentOrders,
+        pendingOrder,
+        totalOrder,
+        cancelledOrder,
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  // End Method
+
+  get_orders = async (req, res) => {
+    // console.log(req.params);
+    const { customerId, status } = req.params;
+    try {
+      let orders = [];
+      if (status !== "all") {
+        orders = await customerOrder.find({
+          customerId: new ObjectId(customerId),
+          delivery_status: status,
+        });
+      } else {
+        orders = await customerOrder.find({
+          customerId: new ObjectId(customerId),
+        });
+      }
+      responseReturn(res, 200, {
+        orders,
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  // End Method
+
+  get_order_details = async (req, res) => {
+    // console.log(req.params);
+    const { orderId } = req.params;
+    try {
+      const order = await customerOrder.findById(orderId);
+      responseReturn(res, 200, {
+        order,
+      });
+    } catch (error) {
+      console.log(error.message);
     }
   };
   // End Method
