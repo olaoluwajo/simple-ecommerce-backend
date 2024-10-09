7f1de08a45e37062868d7c5d83bf8bcbeebdc8a8
olaoluwa <146534182+olaoluwajo@users.noreply.github.com>
2024-10-09 01:25:45 +0100

    sebmit and get reviews method done

--- controllers/home/homeContollers.js
+++ controllers/home/homeContollers.js
@@ -4,6 +4,9 @@ const reviewModel = require("../../models/reviewModel");
 const { responseReturn } = require("../../utils/response");
 const queryProducts = require("../../utils/queryProducts");
 const moment = require("moment");
+const {
+  mongo: { ObjectId },
+} = require("mongoose");
 
 class homeControllers {
   // This makes it easier to display products in rows, for example, in a grid with three products in each row on a webpage!
@@ -253,6 +256,90 @@ class homeControllers {
       });
     } catch (error) {
       console.log("REVIEW ERROR", error.message);
+    }
+  };
+  // end method
+
+  get_reviews = async (req, res) => {
+    //  console.log(req.body);
+    const { productId } = req.params;
+    let { pageNo } = req.query;
+    pageNo = parseInt(pageNo);
+    const limit = 5;
+    const skipPage = limit * (pageNo - 1);
+    try {
+      let getRating = await reviewModel.aggregate([
+        {
+          $match: {
+            productId: {
+              $eq: new ObjectId(productId),
+            },
+            rating: {
+              $not: {
+                $size: 0,
+              },
+            },
+          },
+        },
+        {
+          $unwind: "$rating",
+        },
+        {
+          $group: {
+            _id: "$rating",
+            count: {
+              $sum: 1,
+            },
+          },
+        },
+      ]);
+      let rating_review = [
+        {
+          rating: 5,
+          sum: 0,
+        },
+        {
+          rating: 4,
+          sum: 0,
+        },
+        {
+          rating: 3,
+          sum: 0,
+        },
+        {
+          rating: 2,
+          sum: 0,
+        },
+        {
+          rating: 1,
+          sum: 0,
+        },
+      ];
+      for (let i = 0; i < rating_review.length; i++) {
+        for (let j = 0; j < getRating.length; j++) {
+          if (rating_review[i].rating === getRating[j]._id) {
+            rating_review[i].sum = getRating[j].count;
+            break;
+          }
+        }
+      }
+      const getAll = await reviewModel.find({
+        productId,
+      });
+      const reviews = await reviewModel
+        .find({
+          productId,
+        })
+        .skip(skipPage)
+        .limit(limit)
+        .sort({ createdAt: -1 });
+      responseReturn(res, 200, {
+        reviews,
+        totalReview: getAll.length,
+        rating_review,
+      });
+    } catch (error) {
+      console.log(error.message);
     }
   };
   // end method
